5695869f1422935998433e00a026aa42eb1df4f7
yankchina <yankchina@yankchinamatoiMac.local>
2013-11-03 10:50:21 +0800

    简单的WebServer
    用bottle来建立一个Web服务器

---
+++ Server/Python/TestWebServer.py
@@ -0,0 +1,49 @@
+#!/user/local/bin/python
+#coding=utf-8
+
+
+# 用 bottle 建立一个非常简单的本地测试 Web 服务器
+
+from bottle import route,run,template
+import json
+
+# 测试bottle web server 是否正常运行
+@route('/hello/:name')
+def index(name='world'):
+	return template('<h1>Hello</h1><br/><p>{{name}}</p>',name=name)
+
+# 用户登录，三种角色 teacher:教师 master:督导 student:学生
+@route('/login/:id')
+def index(id):
+	if id == '101011111':
+		title = 'teacher'
+	if id == '202022222':
+		title = 'master'
+	if id == '303033333':
+		title = 'student'
+	value = [{'success':'true'},{'id':id,'title':title}]
+	return json.dumps(value)
+
+
+# 获取本周课表
+@route('/schedule/thisweek')
+def index():
+	courses = []
+	for i in range(5):
+		title = 'title%d'%(i)
+		teacher_id = '1010'
+		teacher_name = 'sample'
+		campus = 'jlh'
+		room = '302'
+		date = 'xq1'
+		time = '1-2'
+		course= {'id':i,'title':title,'teacher_id':teacher_id,'teacher_name':teacher_name,'campus':campus,'room':room,'date':date,'time':time}
+		courses.append(course)
+	return json.dumps(courses)
+
+# 上传评价
+@route('/evaluate/:id/:score/:msg')
+def index(id,score,msg=''):
+	return template('<ul><li>{{id}}</li><li>{{score}}</li><li>{{msg}}</li>',id=id,score=score,msg=msg)
+
+run(host='localhost',port=8080)
